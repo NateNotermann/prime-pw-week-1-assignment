2a0b57bdec04fd483ccf315ffc4548da59d39ae2
Nate Notermann <nate.notermann@gmail.com>
2022-05-04 23:35:30 -0500

    finished all Tasks

---
+++ assignment/helloWorld.js
@@ -0,0 +1,62 @@
+console.log( "hello world" );
+alert ( "Hello Prime!");
+// not gonna lie, I got cocky and was just messing around with stuff..
+
+let TheAnswerToTheultimateQuestionOflifeTheUniverseAndEverythingIs = 42;
+console.log( "The answer to the ultimate question of life, the universe and everything is: ", TheAnswerToTheultimateQuestionOflifeTheUniverseAndEverythingIs ); 
+console.log( "(hitchhiker's guide to the galaxy reference)" );
+
+// MMMMMMMMMMMk.                                                                         ;KMMMMMMMMMMMM
+// MMMMMMMMMMMk.                                                                         ;KMMMMMMMMMMMM
+// MMMMMMMMMMMk.                                                                         ;KMMMMMMMMMMMM
+// MMMMMMMMMMMk.                                                                         ;KMMMMMMMMMMMM
+// MMMMMMMMMMMk.                                                                         ;KMMMMMMMMMMMM
+// MMMMMMMMMMMk.         ....                                               .'..         ;KMMMMMMMMMMMM
+// MMMMMMMMMMMk.       .:OXNKx;...........................................;kKNXO:        ;KMMMMMMMMMMMM
+// MMMMMMMMMMMk.       ,KMMMMMNXKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKXNMMMMM0,       ;KMMMMMMMMMMMM
+// MMMMMMMMMMMk.       .dXMMWKd:::::::::::::::::::::::::::::::::::::::::::dKWMMXo.       ;KMMMMMMMMMMMM
+// MMMMMMMMMMMk.         :KWO'                                             ,OWXc         ;KMMMMMMMMMMMM
+// MMMMMMMMMMMk.         '0Wd.                                             .xW0'         ;KMMMMMMMMMMMM
+// MMMMMMMMMMMk.         '0Wd.                                             .xW0'         ;KMMMMMMMMMMMM
+// MMMMMMMMMMMk.         '0Wd.                                             .xW0'         ;KMMMMMMMMMMMM
+// MMMMMMMMMMMk.         '0Wd.      .:dkxc.                   .lxxd:.      .xW0'         ;KMMMMMMMMMMMM
+// MMMMMMMMMMMk.         '0Wd.     .dNMMMWKkxxxxxxxxxxxxxxxxxkKWMMMNo.     .xW0'         ;KMMMMMMMMMMMM
+// MMMMMMMMMMMk.         '0Wd.     .dWMMMWXkxxxxxxxxxxxxxxxxxkXWMMMWd.     .xW0'         ;KMMMMMMMMMMMM
+// MMMMMMMMMMMk.         '0Wd.      .oXMNx'                   'xNMKl.      .xW0'         ;KMMMMMMMMMMMM
+// MMMMMMMMMMMk.         '0Wd.       .kW0'                     ,KMx.       .xW0'         ;KMMMMMMMMMMMM
+// MMMMMMMMMMMk.         '0Wd.       .xW0'                     ,0Wx.       .xW0'         ;KMMMMMMMMMMMM
+// MMMMMMMMMMMk.         '0Wd.       .xW0'                     ,0Wx.       .xW0'         ;KMMMMMMMMMMMM
+// MMMMMMMMMMMk.         ;KWk.       .xW0'                     :KMk.       .kWK;         ;KMMMMMMMMMMMM
+// MMMMMMMMMMMk.       .lKWMWO;      .xW0'        .',,,,,,,,,;oKWMWO;     :0WMWKl.       ;KMMMMMMMMMMMM
+// MMMMMMMMMMMk.       ;KMMMMWx.     .xW0'        :0XXXXXXXXXNWMMMMWx.   .kWMMMM0,       ;KMMMMMMMMMMMM
+// MMMMMMMMMMMk.       .lKWMWO;      .xW0'        .',,,,,,,,,;oKNWNO;     ;0WMWKl.       ;KMMMMMMMMMMMM
+// MMMMMMMMMMMk.         ;KMk.       .xW0'                     .,,'.       .kMK;         ;KMMMMMMMMMMMM
+// MMMMMMMMMMMk.         '0Wd.       .xW0'                                 .xW0'         ;KMMMMMMMMMMMM
+// MMMMMMMMMMMk.         '0Wd.       .xW0'                                 .xW0'         ;KMMMMMMMMMMMM
+// MMMMMMMMMMMk.         '0Wd.       .kW0'                                 .xW0'         ;KMMMMMMMMMMMM
+// MMMMMMMMMMMk.         '0Wd.      .oXMNx'                                .xW0'         ;KMMMMMMMMMMMM
+// MMMMMMMMMMMk.         '0Wd.     .dWMMMWXkkkkxkkkkkkkkkkkkkkkkkkkkkkkkkkkOXM0'         ;KMMMMMMMMMMMM
+// MMMMMMMMMMMk.         '0Wd.     .oNMMMWKxddddddddddddddddddddddddddddddddddl.         ;KMMMMMMMMMMMM
+// MMMMMMMMMMMk.         '0Wd.      .:dxdc.                                              ;KMMMMMMMMMMMM
+// MMMMMMMMMMMk.         '0Wd.                                                           ;KMMMMMMMMMMMM
+// MMMMMMMMMMMk.         '0Wd.                                                           ;KMMMMMMMMMMMM
+// MMMMMMMMMMMk.         '0Wd.                                                           ;KMMMMMMMMMMMM
+// MMMMMMMMMMMk.         '0Wd.                                                           ;KMMMMMMMMMMMM
+// MMMMMMMMMMMk.         '0Wx.                                                           ;KMMMMMMMMMMMM
+// MMMMMMMMMMMXOxxxxxxxxxONMXkxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx0WMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// KKKKKKXXXNWMMMMMMMMMMMNXKKKKKKXXNWMMMMMMMMMMWXOkOXWWMMMMMMMNXXWMMMMMMMMMMMWXXNMMMMMMMMMNXKKKKKKKKKKX
+// ..''''''',:dKWMMMMMMMNo..''''''',;o0WMMMMMMWO,   ;d0MMMMMMWd..lXMMMMMMMMMNd'.oNMMMMMMMNd..''''''''':
+//  'xOOOOOOd;.'kWMMMMMMX: ,kOOOOOOx:..dNMMMMMWx.   .:kMMMMMMWo  .oNMMMMMMMWk.  cNMMMMMMMNl .dOOOOOOOO0
+//  ;XMMMMMMMX: ;XMMMMMMX: cNMMMMMMMNl ,0MMMMMMNx. 'kXNMMMMMMWl   .kWMMMMMM0,.  cNMMMMMMMNl ,KMMMMMMMMM
+//  ;XMMMMMMMNl ,KMMMMMMX: cNMMMMMMMNl ,0MMMMMMMK; cNMMMMMMMMWl ':.,0MMMMMK:.;, cNMMMMMMMNl ,KWWMMMMMMM
+//  ;KWWWWWNKd..oNMMMMMMX: ;OKKKKK0kc..xWMMMMMMMK; cNMMMMMMMMWl ,Ol.:KMMMNl.:O: cNMMMMMMMNl .,::::xNMMM
+//  .,;;;;;;'';kNMMMMMMMX: .''''....;o0WMMMMMMMMK; cNMMMMMMMMNl ,KK:.lNMNd.,0X: cNMMMMMMMNl .cooookNMMM
+//  .ldddddxOKWMMMMMMMMMX: ;OKKKx' ;0WMMMMMMMMMMK; cNMMMMMMMMWl ,KM0,.dNk.'OWX: cNMMMMMMMNl ,KMMMMMMMMM
+//  ;KMMMMMMMMMMMMMMMMMMX: cNMMMWO'.:KMMMMMMMMMMK; cNMMMMMMMMWo ,KMWk.'l,.xWMX: cNMMMMMMMNl ,KMMMMMMMMM
+//  ;KMMMMMMMMMMMMMMMMMMX: cNMMMMWO' :KMMMMMMMMMK; cNMMMMMMMMWo ,KMMWd. .oNMMX: cNMMMMMMMNl ,KMMMMMMMMM
+//  ;KMMMMMMMMMMMMMMMMMMX: cNMMMMMWO'.:KMMMMMMMMK; cNMMMMMMMMNl ,KMMMNl.cXMMMX: cNMMMMMMMNl .;ccccccccc
+// .lXMMMMMMMMMMMMMMMMMMNo'dNMMMMMMWk,,kWMMMMMMMXl'dWMMMMMMMMWx'cXMMMM0d0MMMMNo'oNMMMMMMMWd'...........
